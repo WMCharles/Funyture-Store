4c1583e1c2e8dc7e7451c236e0cd6e3c34f4b17e
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-05 19:06:51 +0300

    add content

---
+++ src/components/Products.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import "./Products.css"
+
+export default function Products() {
+  return (
+    <div className='products'>
+        <h2>FUNitures</h2>
+        <div className='products-wrapper'>
+                <div className='product-container'>
+                    <div className='product'>
+                        <div className='image'>
+                            <img src='https://images.unsplash.com/photo-1558898434-af897d9ac0a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjN8fHNvZmF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt=''/>
+                        </div>
+                        <div className='title'>
+                            <h3>Sofa</h3>
+                        </div>
+                        <div className='price'>
+                            <h3>Kshs. 60000</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='product-container'>
+                    <div className='product'>
+                        <div className='image'>
+                            <img src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c29mYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' alt=''/>
+                        </div>
+                        <div className='title'>
+                            <h3>Sofa</h3>
+                        </div>
+                        <div className='price'>
+                            <h3>Kshs. 60000</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='product-container'>
+                    <div className='product'>
+                        <div className='image'>
+                            <img src='https://static6.depositphotos.com/1123553/614/i/600/depositphotos_6142155-stock-photo-yellow-sofa-interior-design.jpg' alt=''/>
+                        </div>
+                        <div className='title'>
+                            <h3>Sofa</h3>
+                        </div>
+                        <div className='price'>
+                            <h3>Kshs. 60000</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='product-container'>
+                    <div className='product'>
+                        <div className='image'>
+                            <img src='https://images.unsplash.com/photo-1603204077067-65ee7573cf3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODR8fHNvZmF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt=''/>
+                        </div>
+                        <div className='title'>
+                            <h3>Sofa</h3>
+                        </div>
+                        <div className='price'>
+                            <h3>Kshs. 60000</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='product-container'>
+                    <div className='product'>
+                        <div className='image'>
+                            <img src='https://images.unsplash.com/photo-1540574163026-643ea20ade25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c29mYXxlbnwwfHwwfHw%3D&w=1000&q=80' alt=''/>
+                        </div>
+                        <div className='title'>
+                            <h3>Sofa</h3>
+                        </div>
+                        <div className='price'>
+                            <h3>Kshs. 60000</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='product-container'>
+                    <div className='product'>
+                        <div className='image'>
+                            <img src='https://static6.depositphotos.com/1123553/614/i/600/depositphotos_6142155-stock-photo-yellow-sofa-interior-design.jpg' alt=''/>
+                        </div>
+                        <div className='title'>
+                            <h3>Sofa</h3>
+                        </div>
+                        <div className='price'>
+                            <h3>Kshs. 60000</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='product-container'>
+                    <div className='product'>
+                        <div className='image'>
+                            <img src='https://images.unsplash.com/photo-1491926626787-62db157af940?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHNvZmF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt=''/>
+                        </div>
+                        <div className='title'>
+                            <h3>Sofa</h3>
+                        </div>
+                        <div className='price'>
+                            <h3>Kshs. 60000</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='product-container'>
+                    <div className='product'>
+                        <div className='image'>
+                            <img src='https://images.unsplash.com/photo-1634712282287-14ed57b9cc89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNvZmF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt=''/>
+                        </div>
+                        <div className='title'>
+                            <h3>Sofa</h3>
+                        </div>
+                        <div className='price'>
+                            <h3>Kshs. 60000</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className='product-container'>
+                    <div className='product'>
+                        <div className='image'>
+                            <img src='https://images.unsplash.com/photo-1512212621149-107ffe572d2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNvZmF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt=''/>
+                        </div>
+                        <div className='title'>
+                            <h3>Sofa</h3>
+                        </div>
+                        <div className='price'>
+                            <h3>Kshs. 60000</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+  )
+}
