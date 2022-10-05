b16dfc8480b55b04a1df8da40894372dc6cf6f3d
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-05 15:41:16 +0300

    import components & add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React, {BrowserRouter as Router, Routes, Route} from 'react';
+import Cart from './components/Cart';
+import Products from './components/Products';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Contact from './components/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="products" element={<Products/>}/>
+        <Route path="cart" element={<Cart/>}/>
+        <Route path="contact" element={<Contact/>}/>
+      </Routes>
+    </Router>
   );
 }
 
