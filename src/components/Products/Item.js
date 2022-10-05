1115e9463da826d2138a7e2f0eabc714cf13b129
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-05 23:17:01 +0300

    add dynamic data

--- src/components/Products/Item.js
+++ src/components/Products/Item.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import "./Item.css"
+import { useParams } from 'react-router-dom'
 
-export default function Item() {
-  return (
-    <div className='product'>
-        <h1>Product Details</h1>
-        <div className='item'>
-            <div className='image'>
-                <img src="https://static6.depositphotos.com/1123553/614/i/600/depositphotos_6142155-stock-photo-yellow-sofa-interior-design.jpg" alt="" />
-            </div>
-            <div className='description'>
-                <h2>Product Title</h2>
-                <h3>$ 350</h3>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                </p>
-                <button>Add to Cart</button>
-            </div>
+export default function Item({itemData}) {
+    const {title} = useParams()
+    return (
+        <div className='product'>
+            <h1>Product Details</h1>
+            {itemData.filter((item) => item.title === title).map((product) =>
+                <div className='item'>
+                    <div className='image'>
+                        <img src={product.image} alt="" />
+                    </div>
+                    <div className='description'>
+                        <h2>{product.title}</h2>
+                        <h3>$ {product.price}</h3>
+                        <p>{product.description}</p>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            )}
         </div>
-    </div>
-  )
+    )
 }
