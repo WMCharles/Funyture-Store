7448ee5d6cf2a4ee8df4fb8467453b3f8450e6b2
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-06 12:18:33 +0300

    handle form to submit data to server & update state

--- src/components/Products/AddProduct.js
+++ src/components/Products/AddProduct.js
@@ -1,29 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./AddProduct.css"
 
-export default function AddProduct() {
-  return (
-    <div className='Product'>
-        <h1>Add Product</h1>
-        <div className='AddProduct'>
-            <form>
-                <div className='input-control'>
-                    <label>Name</label>
-                    <input type="text" required/>
-                </div>
-                <div className='input-control'>
-                    <label>Price</label>
-                    <input type="number" required/>
-                </div>
-                <div className='input-control'>
-                    <label>Description</label>
-                    <textarea cols="" rows="5"></textarea>
-                </div>
-                <div className='input-control'>
-                    <button type='submit'>Add Product</button>
-                </div>
-            </form>
+export default function AddProduct({addToProducts}) {
+    const [formData, setFormData] = useState({
+        title: "",
+        price:"",
+        description:"",
+        image:""
+
+    })
+    function handleInputChange(event){
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+        fetch("https://blowg.herokuapp.com/products", {
+            method: "POST",
+            headers: {
+                "content-type":"application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(res => res.json())
+        .then((item) => addToProducts(item))        
+    }
+
+    return (
+        <div className='Product'>
+            <h1>Add Product</h1>
+            <div className='AddProduct'>
+                <form onSubmit={handleSubmit}>
+                    <div className='input-control'>
+                        <label>Title</label>
+                        <input type="text" required name='title' onChange={handleInputChange} value={formData.title}/>
+                    </div>
+                    <div className='input-control'>
+                        <label>Price</label>
+                        <input type="number" name='price' required onChange={handleInputChange} value={formData.price}/>
+                    </div>
+                    <div className='input-control'>
+                        <label>Description</label>
+                        <textarea cols="" rows="5" name='description' required onChange={handleInputChange} value={formData.description}></textarea>
+                    </div>
+                    <div className='input-control'>
+                        <label>Image URL</label>
+                        <input type="text" name='image' required onChange={handleInputChange} value={formData.image}/>
+                    </div>
+                    <div className='input-control'>
+                        <button type='submit'>Add Product</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
   )
 }
