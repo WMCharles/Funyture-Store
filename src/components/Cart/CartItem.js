fff387017beb35a5c836d33590dc2a416f6ad3d0
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-06 10:34:01 +0300

    add onClick listener to remove button

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,26 +1,29 @@
 import React from 'react'
 
-export default function CartItem({item}) {
-  return (
-    <div className='item'>
-        <div className='image'>
-            <img src={item.image} alt="" />
-        </div>
-        <div className='description'>
-            <div>
-              <h3>{item.title}</h3>
-              <br/>
-              <h3 className='price'>$ {item.price}</h3>
+export default function CartItem({item, removeItem}) {
+    function removeItemFromCart(){
+        removeItem(item)
+    }
+    return (
+        <div className='item'>
+            <div className='image'>
+                <img src={item.image} alt="" />
+            </div>
+            <div className='description'>
+                <div>
+                <h3>{item.title}</h3>
+                <br/>
+                <h3 className='price'>$ {item.price}</h3>
+                </div>
+            </div>
+            <div className='quantity'>
+                <button>+</button> 1 <button>-</button>
+            </div>
+            <div className='total-amount'>
+                <h2>$ {item.price}</h2>
+                <br/>
+                <p onClick={() => removeItemFromCart(item)}>Remove</p>
             </div>
         </div>
-        <div className='quantity'>
-            <button>+</button> 1 <button>-</button>
-        </div>
-        <div className='total-amount'>
-            <h2>$ {item.price}</h2>
-            <br/>
-            <p>Remove</p>
-        </div>
-    </div>
-  )
+    )
 }
