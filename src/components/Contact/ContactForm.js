058074d46291fc4fd610042edc3c54de083fa6fa
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-06 12:45:40 +0300

    add Contact form component

---
+++ src/components/Contact/ContactForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+export default function ContactForm() {
+    const [formData, setFormData] = useState({
+        name:"",
+        email:"",
+        message:""
+    })
+
+    function handleInputChange(e){
+        setFormData ({...formData, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        fetch("https://blowg.herokuapp.com/messages", {
+            method: "POST",
+            headers: {
+                "content-type":"application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then((res) => res.json())
+        .then((item) => console.log(item))
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className='input-control'>
+                <label>Name</label>
+                <input type="text" name="name" onChange={handleInputChange} required/>
+            </div>
+            <div className='input-control'>
+                <label>Email</label>
+                <input type="email" name='email'  onChange={handleInputChange} required/>
+            </div>
+            <div className='input-control'>
+                <label>Message</label>
+                <textarea cols="" rows="5" name='message'  onChange={handleInputChange} required></textarea>
+            </div>
+            <div className='input-control'>
+                <button type='submit'>Submit</button>
+            </div>
+        </form>
+    )
+}
