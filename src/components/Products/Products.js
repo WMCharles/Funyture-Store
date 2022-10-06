bdd44ce221b7837e43a81fb592cb95856efaf12a
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-06 11:30:09 +0300

    add useNavigate

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import "./Products.css"
 import Product from './Product'
+import { useNavigate } from "react-router-dom"
 
 export default function Products({products}) {
+  const navigate = useNavigate()
+
   return (
     <div className='products'>
         <div className='headers'>
           <h1>FUNITURES</h1>
-          <h3>ADD FUNITURES</h3>
+          <h3 onClick={() => {navigate('/products/addproduct')}}>ADD FUNITURES</h3>
         </div>
         <div className='products-wrapper'>
           {products.map((product, index) =>
