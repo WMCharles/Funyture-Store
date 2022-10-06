10182c72ff55dde03630b467fb9941df9144ac55
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-06 10:19:14 +0300

    add CartItem component

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function CartItem({item}) {
+  return (
+    <div className='item'>
+        <div className='image'>
+            <img src={item.image} alt="" />
+        </div>
+        <div className='description'>
+            <div>
+              <h3>{item.title}</h3>
+              <br/>
+              <h3 className='price'>$ {item.price}</h3>
+            </div>
+        </div>
+        <div className='quantity'>
+            <button>+</button> 1 <button>-</button>
+        </div>
+        <div className='total-amount'>
+            <h2>$ {item.price}</h2>
+            <br/>
+            <a href="#">Remove</a>
+        </div>
+    </div>
+  )
+}
