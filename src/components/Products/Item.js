58c3bce7fc054edff98151a062a9df8f7aeda832
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-06 09:03:51 +0300

    add addToCart props & onClick listener

--- src/components/Products/Item.js
+++ src/components/Products/Item.js
@@ -2,13 +2,16 @@ import React from 'react'
 import "./Item.css"
 import { useParams } from 'react-router-dom'
 
-export default function Item({itemData}) {
+export default function Item({itemData, addToCart}) {
     const {title} = useParams()
+    function handleAddToCart(item){
+        addToCart(item)
+    }
     return (
         <div className='Product'>
             <h1>Product Details</h1>
-            {itemData.filter((item) => item.title === title).map((product) =>
-                <div className='Item'>
+            {itemData.filter((item) => item.title === title).map((product, index) =>
+                <div className='Item' key={index}>
                     <div className='productImage'>
                         <img src={product.image} alt="" />
                     </div>
@@ -16,7 +19,7 @@ export default function Item({itemData}) {
                         <h2>{product.title}</h2>
                         <h3>$ {product.price}</h3>
                         <p>{product.description}</p>
-                        <button>Add to Cart</button>
+                        <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
                     </div>
                 </div>
             )}
