1546af5b96e9b84a163fde240fb58825c2eb7543
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-06 21:43:54 +0300

    add functions that make prices dynamic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
 import "./Cart.css"
 import CartItem from './CartItem'
 
-export default function Cart({cart, removeItem}) {
+export default function Cart({cart, removeItem, handleChange}) {
+  const [price, setPrice] = useState(0)
+
+  function handlePrice(){
+    let total = 0
+    {cart.map((item) => (total += item.price * item.quantity))}
+    setPrice(total)
+  }
+  
+  useEffect(() => {
+    handlePrice()
+  })
   return (
     <div className='cart'>
       <h1>Shopping Cart</h1>
-      {cart.map((item) => 
-        <CartItem item={item} removeItem={removeItem}/>
+      {cart.map((item, index) => 
+        <CartItem item={item} removeItem={removeItem} key={index} handleChange={handleChange}/>
       )}
       <hr/>
       <div className='net'>
@@ -15,7 +27,7 @@ export default function Cart({cart, removeItem}) {
         <div></div>
         <div></div>
         <div className='checkout'>
-          <h1>$ Total</h1>
+          <h1>$ {price}</h1>
           <button>Checkout</button>
         </div>
       </div>
